dbc8aa1937f78c6b63d3bb60f51a0a5abfc43d58
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-08 16:11:50 -0400

    Form creates new listing, routes you to dashboard

--- src/components/CreateListing/CreateListing.jsx
+++ src/components/CreateListing/CreateListing.jsx
@@ -1,8 +1,32 @@
 import CreateListingForm from './CreateListingForm'
+import { useNavigate } from 'react-router-dom';
 
-const CreateListing = () => {
+const CreateListing = ({ token }) => {
+
+  const COHORT_NAME = '2306-FSA-ET-WEB-FT-SF';
+  const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+
+  const navigate = useNavigate();
+
+  const makeListing = async (input) => {
+    try {
+      const response = await fetch(`${BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(input)
+      });
+      const result = await response.json();
+      navigate('/user/dashboard');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <CreateListingForm />
+    <CreateListingForm makeListing={makeListing}/>
   )
 }
 
--- src/components/CreateListing/CreateListingForm.jsx
+++ src/components/CreateListing/CreateListingForm.jsx
@@ -1,6 +1,89 @@
-const CreateListingForm = () => {
+import { useState } from 'react'
+
+const CreateListingForm = ({ makeListing }) => {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [location, setLocation] = useState();
+  const [willDeliver, setWillDeliver] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const body = {
+      post: {
+        title,
+        description,
+        price,
+        location,
+        willDeliver
+      }
+    }
+    makeListing(body)
+  }
+
   return (
-    <h1>listing form</h1>
+    <form onSubmit={submitHandler}>
+      <label htmlFor='title'>title</label>
+        <input 
+          required
+          name='title'
+          id='title'
+          placeholder='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      <label htmlFor='description'>description</label>
+        <input 
+          required
+          name='description'
+          id='description'
+          placeholder='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label htmlFor='price'>price</label>
+        <input 
+          required
+          name='price'
+          id='price'
+          placeholder='price'
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <label htmlFor='location'>location</label>
+        <input 
+          name='location'
+          id='location'
+          placeholder='location'
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <label>Will you deliver?</label>
+        <label>
+          <input
+            required
+            type='radio'
+            value={true}
+            name='willDeliver'
+            // checked={willDeliver === true}
+            onChange={(event) => setWillDeliver(event.target.value)}
+          />
+         Yes
+        </label>
+        <label>
+        <input
+            required
+            type='radio'
+            value={false}
+            name='willDeliver'
+            // checked={willDeliver === false}
+            onChange={(event) => setWillDeliver(event.target.value)}
+          />
+         No
+        </label>
+        <button>Create</button>
+    </form>
   )
 }
 
